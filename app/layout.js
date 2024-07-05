3361fb4a8c6d38ddf627182695432e1b36185a5c
tanmay-sketch <grandhi1@msu.edu>
2024-07-05 14:37:13 +0530

    Added the logo

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icons/logo.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/icons/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icons/logo.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
