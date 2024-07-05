6cefe3b94bcb6a2fbb3f86108de20477ef093e8e
tanmay-sketch <grandhi1@msu.edu>
2024-07-05 13:40:00 +0530

    Added waitlist button

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -8,9 +8,14 @@ export default function Hero() {
         <h1 className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 text-5xl sm:text-6xl md:text-7xl font-bold mb-4 text-center">
           Welcome to learnbackprop!
         </h1>
-        <h3 className="text-lg sm:text-xl md:text-2xl font-normal text-center">
+        <h3 className="text-lg sm:text-xl md:text-2xl font-normal text-center mb-6">
           Learn backpropagation visually and interactively
         </h3>
+        <a href="https://ohk8z0l81b7.typeform.com/to/cFCDtLkZ" target="_blank" rel="noopener noreferrer">
+          <button className="bg-blue-600 text-white py-2 px-4 rounded text-lg md:text-xl hover:bg-blue-700 transition-colors duration-300 mt-4">
+            Join the Waitlist
+          </button>
+        </a>
       </div>
     </div>
   );
